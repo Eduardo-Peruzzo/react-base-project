71e92ef9af3169a0ce2c59df2672d8f1690397f9
Eduardo Peruzzo <eduardo.peruzzo05@gmail.com>
2024-10-03 17:00:29 -0300

    atualizações página nova

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,13 @@
+import Base from "./Base"
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+
+const Nova = () => {
+    return (
+        <Base> <h1>Nova página</h1>
+        <p>Oi</p>
+        <NovoComponente />
+        </Base>
+      );
+}
+
+export default Nova;
